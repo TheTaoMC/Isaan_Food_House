8d0d6bf9c8758cb31a05d0a3957732f974e82068
TheTaoMC <manamanote@gmail.com>
2023-10-17 00:19:58 +0700

    17.10.23.0.19

--- ApiServer/Server.js
+++ ApiServer/Server.js
@@ -3,13 +3,13 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 // get the client
-//const mysql = require('mysql2');
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
-
-
+const secret = 'jwt';
+const jsonParser = bodyParser.json();
 
 // create the connection to database
 /* const connection = mysql.createConnection({
@@ -24,86 +24,94 @@ const conn = mysql.createConnection(process.env.DATABASE_URL);
 console.log(process.env.DATABASE_URL);
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
+app.get('/api/users', function (req, res, next) {
+  conn.query('SELECT * FROM users', function (err, results, fields) {
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+    res.json(results);
+  });
+});
 
-app.get('/users', function (req, res, next) {
-    conn.query(
-        'SELECT * FROM users',
-        function (err, results, fields) {
-            console.log(results); // results contains rows returned by server
-            console.log(fields); // fields contains extra meta data about results, if available
-            res.json(results)
+app.get('/api/products', function (req, res, next) {
+  // simple query
+  conn.query('SELECT * FROM products', function (err, results, fields) {
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+    res.json(results);
+  });
+});
+
+app.get('/api/products/:name', function (req, res, next) {
+  const name = req.params.name;
+  // simple query
+  conn.query(
+    'SELECT * FROM products where name like ?',
+    '%' + [name] + '%',
+    function (err, results, fields) {
+      console.log(results); // results contains rows returned by server
+      //console.log(fields); // fields contains extra meta data about results, if available
+      res.json(results);
+    },
+  );
+});
+
+// POST /register gets JSON bodies
+app.post('/api/register', jsonParser, function (req, res, next) {
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    conn.execute(
+      'insert into users (fname,lname,email,username,password) values (?,?,?,?,?)',
+      [req.body.fname, req.body.lname, req.body.email, req.body.username, hash],
+      function (err, results, fields) {
+        if (err) {
+          res.json({status: 'error', message: err});
+          return;
         }
+        res.json({status: 'ok'});
+      },
     );
+  });
 });
 
-app.get('/products', function (req, res, next) {
-    // simple query
-    conn.query(
-        'SELECT * FROM products',
-        function (err, results, fields) {
-            console.log(results); // results contains rows returned by server
-            console.log(fields); // fields contains extra meta data about results, if available
-            res.json(results)
-        }
-    );
+// POST /login
+app.post('/api/login', jsonParser, function (req, res, next) {
+  conn.execute(
+    'select * from users where username = ?',
+    [req.body.username],
+    function (err, results, fields) {
+      if (err) {
+        res.json({status: 'error', message: err});
+        return;
+      }
+      if (results.length === 0) {
+        res.json({status: 'error', message: 'user not found!!!'});
+        return;
+      }
+      bcrypt.compare(
+        req.body.password,
+        results[0].password,
+        function (err, result) {
+          if (result) {
+            const token = jwt.sign({username: results[0].username}, secret, {
+              expiresIn: 60 * 15,
+            });
+            res.json({status: 'ok', message: 'login success', token});
+            return;
+          } else {
+            res.json({status: 'error', message: 'login failed'});
+            return;
+          }
+        },
+      );
+    },
+  );
 });
 
-app.get('/products/:name', function (req, res, next) {
-    const name = req.params.name
-    // simple query
-    conn.query(
-        'SELECT * FROM products where name like ?', '%' + [name] + '%',
-        function (err, results, fields) {
-            console.log(results); // results contains rows returned by server
-            //console.log(fields); // fields contains extra meta data about results, if available
-            res.json(results)
-        }
-    );
+// POST /authen
+app.post('/api/authen', jsonParser, function (req, res, next) {
+    //****** */
 });
-
-
-// กำหนดเส้นทางสำหรับ API Login ของคุณ
-app.post('/login', async (req, res) => {
-    // รับข้อมูลจากผู้ใช้
-    const username = req.body.username;
-    const password = req.body.password;
-
-    // ตรวจสอบว่าผู้ใช้มีอยู่หรือไม่
-    const sql = 'SELECT * FROM users WHERE username = ?';
-    const results = await conn.query(sql, [username]);
-    const user = results.length > 0 ? results[0] : null;
-
-    // ตรวจสอบว่ารหัสผ่านถูกต้องหรือไม่
-    if (user && bcrypt.compareSync(password, user.password)) {
-        // สร้าง token
-        const token = bcrypt.hashSync(username + Date.now(), 10);
-
-        // บันทึก token ลงในฐานข้อมูล
-        const sql = 'UPDATE users SET token = ? WHERE id = ?';
-        await conn.query(sql, [token, user.id]);
-
-        // ส่ง token กลับไปให้ผู้ใช้
-        res.status(200).json({
-            status: 'ok',
-            message: 'Logged in',
-            accessToken: token,
-            expiresIn: 60000,
-            user: user
-        });
-    } else {
-        // ส่งข้อผิดพลาดกลับไปให้ผู้ใช้
-        res.status(401).json({
-            status: 'error',
-            message: 'Login failed'
-        });
-    }
-});
-
 
 app.listen(89, function () {
-    console.log('CORS-enabled web server listening on port 89');
+  console.log('CORS-enabled web server listening on port 89');
 });
